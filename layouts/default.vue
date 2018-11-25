fe9f2d43685ea678b7144d78fc2ab5b8dfe87208
Rick Booth <rixcy.rixcy@gmail.com>
2018-11-25 11:40:10 +0000

    Remove original index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,10 @@
 <template>
   <div id="app">
-    <!-- <Nav /> -->
     <nuxt/>
   </div>
 </template>
 
 <script>
-import Nav from '~/components/Nav.vue'
-
-export default {
-  components: {
-    Nav
-  }
-}
 </script>
 
 <style>
